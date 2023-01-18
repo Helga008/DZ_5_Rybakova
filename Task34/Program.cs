4ed64436e600089128aaad77c0bfd420541d810b
Olga <olgarybakova@bk.ru>
2023-01-18 17:45:52 +0300

    Задача 34

---
+++ Task34/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] GetArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+
+    }
+    return array;
+}
+
+int getIntFromUser(string message)
+{
+    Console.WriteLine(message);
+    int userInt = int.Parse(Console.ReadLine());
+    return userInt;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+int CountEvenNumber (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length1 = getIntFromUser("Введите размер массива: ");
+int[] mass =  GetArray(length1);
+PrintArray(mass);
+int countnumb = CountEvenNumber(mass);
+Console.WriteLine();
+Console.WriteLine(countnumb);
